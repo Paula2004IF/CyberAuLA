47a2a1c2c55130a35ed2c82c49bc2ef05656226b
Paula2004IF <lucianedepaula2004@gmail.com>
2024-12-05 20:32:45 +0000

    05122024dicionario.py

---
+++ 05122024desafio.py
@@ -0,0 +1,15 @@
+'''Faça um programa de cadastramento de pessoas com os seguintes requisitos:
+
+1 - O programa mostra um menu de opções em loop. As opções são:
+    1 - Cadastrar
+    2 - Buscar
+    3 - Remover
+2 - Na opção Cadastrar, o usuário insere um CPF no formato XXX.XXX.XXX-XX,
+    um nome, um email e uma data de nascimento no formato DIA/MES/ANO, e
+    estes dados são cadastrados.
+3 - Na opção Buscar, o usuário insere um CPF de um cadastro e o programa
+    mostra os dados da pessoa com esse CPF (nome, email e data de nascimento).
+3 - Na opção Remover, o usuário insere um CPF de um cadastro e o programa
+    deleta esse cadastro.
+
+Utilize um dicionário de tuplas para registrar os cadastros.'''
---
+++ 05122024dicionario.py
@@ -0,0 +1,13 @@
+meu_dicionario = {
+    '000.000.000.-00':'Fulano de Tal', 
+    '111.111.111-11':'Beltrano da Silva',
+    '222.222.222-22' : 'Ciclano Oliveira'}
+#print(meu_dicionario)
+
+#INSERIR DADOS NO DICIONÁRIO:
+#meu_diconario[333.333.333=33]= 'Zulano Carvalho'
+
+
+CPF = input('CPF: ')
+nome = meu_dicionario[CPF]
+print(nome)
